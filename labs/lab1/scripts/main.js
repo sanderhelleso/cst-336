c3b30db478ce2aa6e516279c76977491039b3bc4
Sander Hellesoe <shellesoe@csumb.edu>
2018-09-04 00:11:33 +0000

    added files

---
+++ labs/lab1/scripts/main.js
@@ -0,0 +1,43 @@
+window.onload =  start;
+
+function start() {
+    const heading = document.querySelector('h1');
+    heading.innerHTML = addSpans(heading);
+    
+    // run emoji gallery if index file
+    const url = window.location.href.split('/');
+    if (url[url.length - 1].split('.')[0] === 'index') {
+            emoji(document.querySelector('#emoji'));
+    }
+}
+
+// add a span around every other letter
+function addSpans(ele) {
+    let count = 0;
+    let formatedHeading = ''
+    ele.innerHTML.split('').forEach(letter => {
+        count++;
+        if (count % 2 === 0) {
+            formatedHeading += `<span>${letter}</span>`;
+        }
+        
+        else {
+            formatedHeading += letter;
+        }
+    });
+    
+    return formatedHeading;
+    
+}
+
+function emoji(ele) {
+    const emojis = ['&#x1F436;', '&#128565;', '&#128585;', '&#128519;', '&#129302;', '&#128169;', '&#128591;'];
+    let count = 0;
+    setInterval(() => {
+        ele.innerHTML = emojis[count];
+        count++;
+        if (count === emojis.length) {
+            count = 0;
+        }
+    }, 200);
+}
