904eb2362a18a1ad0dacd54e61050dd846506bbf
Sander Hellesoe <shellesoe@csumb.edu>
2018-09-14 04:14:57 +0000

    finished lab2

--- labs/lab2/js/index.js
+++ labs/lab2/js/index.js
@@ -8,7 +8,7 @@ function jackpot() {
         localStorage.setItem('points', parseInt(localStorage.getItem('points')) + parseInt(points.innerHTML));
         localStorage.setItem('attempts', parseInt(localStorage.getItem('attempts')) + parseInt(attempts.innerHTML));
         
-        console.log(localStorage.getItem('points');
+        console.log(localStorage.getItem('points'));
         
         points.innerHTML = localStorage.getItem('points');
         attempts.innerHTML = localStorage.getItem('attempts');
