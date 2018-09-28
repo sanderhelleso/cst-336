654768909810538e49664f54388ba9b9b37915ec
Sander Hellesoe <shellesoe@csumb.edu>
2018-09-28 21:52:05 +0000

    added lab 4

---
+++ labs/lab4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="./css/styles.css" type="text/css" />
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <form>
+            <input type="text" name=""/>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
+</html>
---
+++ practice/htmlForm/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form>
+            <p>Name:</p>
+            <input type="text" name=""/>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
+</html>
