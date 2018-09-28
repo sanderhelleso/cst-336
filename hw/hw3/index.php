3f2c5a9d225ccbd16ba3ce447c312cb976350afe
Sander Hellesoe <shellesoe@csumb.edu>
2018-09-28 02:51:47 +0000

    added hw3 folder

---
+++ hw/hw3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/style.css" type="text/css" />
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <main>
+            <?php
+                include './inc/form.php';
+            ?>
+        </main>
+    </body>
+</html>
