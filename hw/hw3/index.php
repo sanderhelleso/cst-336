753cbe2d2af8ac8ea946207e272a8ee6d429d74c
Sander Hellesoe <shellesoe@csumb.edu>
2018-09-28 07:14:49 +0000

    added js and started on animations

--- hw/hw3/index.php
+++ hw/hw3/index.php
@@ -2,22 +2,31 @@
 <html>
     <head>
         <link rel="stylesheet" href="./css/style.css" type="text/css" />
+        <link rel="stylesheet" href="./css/animate.css" type="text/css" />
+        <script type="text/javascript" src="./js/main.js"></script>
         <title>Hello World</title>
     </head>
     <body>
         <main>
             <div class="grid-container">
                 <div class="grid-item">
-                    <h4 id="form-heading-intro">DANCECLUB</h4>
-                    <h1 id="form-heading" class="long-shadow">Futur<span>a</span></h1>
-                    <p id="form-intro">I know this pretty rave girl, Always think about her, When she says hi to me, Butterflies go right through me, And when I see her dancing, Wanna take a chance in, Getting a little closer, And maybe get to know her</p>
-                    <a id="form-button" class="long-shadow">Sign Up Today</a>
-                    <a id="got-account">Allready a member?</a>
-                    <?php
-                        include './inc/form.php';
-                    ?> 
+                    <div id="intro">
+                        <h4 id="form-heading-intro" class="animated fadeInDown">NIGHTCLUB</h4>
+                        <h1 id="form-heading" class="long-shadow animated fadeInUp">Futur<span>a</span></h1>
+                        <p id="form-intro" class="animated fadeIn">I know this pretty rave girl, Always think about her, When she says hi to me, Butterflies go right through me, And when I see her dancing, Wanna take a chance in, Getting a little closer, And maybe get to know her</p>
+                        <a id="form-button" class="long-shadow">Sign Up Today</a>
+                        <a id="got-account">Allready a member?</a>
+                    </div>
+                    <div id="form" class="animated fadeIn">
+                        <h1 id="form-heading-stage2" class="animated fadeInUp">Can you feel the energy?</h1>
+                        <form>
+                            <input type="text" name="test1"/>
+                            <input type="text" name="test2"/>
+                            <input type="text" name="test3"/>
+                        </form>
+                    </div>
                 </div>
-                <div id="form-landing-image" class="grid-item">
+                <div id="form-landing-image" class="grid-item animated fadeIn">
                     
                 </div>
             </div>
---
+++ hw/hw3/js/main.js
@@ -0,0 +1,49 @@
+window.onload = start;
+
+function start() {
+    document.querySelector('#form-button').addEventListener('click', animate);
+}
+
+function animate() {
+    
+    // initial fade out
+    document.querySelector('#form-heading-intro').className = 'animated fadeOutUp';
+    document.querySelector('#form-heading').className = 'long-shadow animated fadeOutDown';
+    document.querySelector('#form-intro').className = 'animated fadeOut';
+    
+    // post fade out of button
+    this.className = 'long-shadow animated bounceOut';
+    //this.parentElement.className = 'grid-item animated fadeOutLeft';
+    document.querySelector('#got-account').className = 'animated fadeOutDown';
+    
+    // animate image
+    document.querySelector('#form-landing-image').className = 'grid-item animated fadeOut';
+    
+    setTimeout(() => {
+        showForm();
+    }, 1000);
+}
+
+function showForm() {
+    document.querySelector('#intro').style.display = 'none';
+    document.body.className = 'animated fadeIn dark';
+    document.querySelector('#form').style.display = 'block';
+    
+    // manipulate cover
+    const cover = document.querySelector('#form-landing-image');
+    cover.style.background = 'linear-gradient(to right, rgba(0, 0, 0, 1),rgb(0, 0, 0, 0.4)),url("./img/cover2.jpg")';
+    cover.style.backgroundPosition = 'center';
+    cover.style.backgroundRepeat = 'none';
+    cover.style.backgroundSize = 'cover';
+    cover.className = 'grid-item animated fadeIn';
+    setTimeout(() => {
+        let timer = 250;
+        Array.from(document.querySelector('form').querySelectorAll('input')).forEach(input => {
+            setTimeout(() => {
+                input.className = 'animated fadeInUp';
+                input.style.display = 'block';
+            }, timer);
+            timer += 250;
+        })
+    }, 250);
+}
