b9e0e413c8fc7978cdf9e93aff86af7c55c4c277
Sander Hellesoe <shellesoe@csumb.edu>
2018-09-28 05:26:50 +0000

    added main style

--- hw/hw3/index.php
+++ hw/hw3/index.php
@@ -5,11 +5,22 @@
         <title>Hello World</title>
     </head>
     <body>
-        <h1>Hello World</h1>
         <main>
-            <?php
-                include './inc/form.php';
-            ?>
+            <div class="grid-container">
+                <div class="grid-item">
+                    <h4 id="form-heading-intro">DANCECLUB</h4>
+                    <h1 id="form-heading" class="long-shadow">Futur<span>a</span></h1>
+                    <p id="form-intro">I know this pretty rave girl, Always think about her, When she says hi to me, Butterflies go right through me, And when I see her dancing, Wanna take a chance in, Getting a little closer, And maybe get to know her</p>
+                    <a id="form-button" class="long-shadow">Sign Up Today</a>
+                    <a id="got-account">Allready a member?</a>
+                    <?php
+                        include './inc/form.php';
+                    ?> 
+                </div>
+                <div id="form-landing-image" class="grid-item">
+                    
+                </div>
+            </div>
         </main>
     </body>
 </html>
