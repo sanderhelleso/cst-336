152ca5b49f5facca7bfed2bc33733d2e5ac155ad
Sander Hellesoe <shellesoe@csumb.edu>
2018-10-03 20:31:51 +0000

    added lab5 files

---
+++ labs/lab5/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <title>Products Page</title>
+    </head>
+    <body>
+    <div class='container'>
+        <div class='text-center'>
+            
+            <!-- Bootstrap Navagation Bar -->
+            <nav class='navbar navbar-default - navbar-fixed-top'>
+                <div class='container-fluid'>
+                    <div class='navbar-header'>
+                        <a class='navbar-brand' href='#'>Shopping Land</a>
+                    </div>
+                    <ul class='nav navbar-nav'>
+                        <li><a href='index.php'>Home</a></li>
+                        <li><a href='scart.php'>Cart</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <br /> <br /> <br />
+            
+            <!-- Search Form -->
+            <form enctype="text/plain">
+                <div class="form-group">
+                    <label for="pName">Product Name</label>
+                    <input type="text" class="form-control" name="query" id="pName" placeholder="Name">
+                </div>
+                <input type="submit" value="Submit" class="btn btn-default">
+                <br /><br />
+            </form>
+            
+            <!-- Display Search Results -->
+            
+        </div>
+    </div>
+    </body>
+</html>
