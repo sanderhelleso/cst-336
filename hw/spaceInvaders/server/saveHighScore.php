3f4340050042f08a7e16ea20f02d8e227bc3d0d2
Sander Hellesoe <shellesoe@csumb.edu>
2018-11-14 07:13:40 +0000

    added final homewrok 4

--- hw/spaceInvaders/server/saveHighScore.php
+++ hw/spaceInvaders/server/saveHighScore.php
@@ -1,10 +1,8 @@
 <?php
     if (isset($_POST["data"])) {
-        print_r($_POST);
         $file = "../public/highscores/highscores.json";
         $fh = fopen($file, 'w') or die("can't open file");
         $stringData = $_POST["data"];
-        print_r($stringData);
         fwrite($fh, $stringData);
         fclose($fh);
     }
